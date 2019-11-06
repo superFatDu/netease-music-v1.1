d0b76c63fb0739adb498afb67bfa96fce3223065
duchengwu <duchengwu@ccx.cn>
2019-11-06 09:27:20 +0800

    add new interface.

--- src/fetch/api.js
+++ src/fetch/api.js
@@ -1,7 +1,8 @@
 import { ajaxRequest } from './axios.config';
 
 const LOG_IN = {
-  login: param => ajaxRequest('post', '/api/login/cellphone', 1, param)
+  login: param => ajaxRequest('post', '/api/login/cellphone', 1, param),
+  loginStatus: param => ajaxRequest('post', '/api/', 1, param)
 }
 
 export {
