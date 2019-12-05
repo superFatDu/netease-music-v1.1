a2b45acab64ef44a6d5cccfc5f05ff39c082c9db
duchengwu <superduchengwu@163.com>
2019-12-05 21:24:24 +0800

    ✨ add new SongList page.

--- src/router/index.js
+++ src/router/index.js
@@ -31,6 +31,11 @@ const routes = [
     path: '/search',
     name: 'Search',
     component: () => import('../views/Search.vue')
+  },
+  {
+    path: '/songlist/:id',
+    name: 'SongList',
+    component: () => import('../views/SongList.vue')
   }
 ];
 
---
+++ src/views/SongList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="song-list">
+    <div class="list-header">
+      <back-header></back-header>
+    </div>
+  </div>
+</template>
+
+<script>
+import BackHeader from '../components/BackHeader.vue'
+export default {
+  name: 'SongList',
+  components: {
+    BackHeader
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
